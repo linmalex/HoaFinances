8e83c8eb9ebb645eff5eaefe8bbc8e3a55873fb2
Lindsay Alexander <linmalex@gmail.com>
2020-09-17 22:46:30 -0700

    add startup class

---
+++ HoaTreasurerFunctions/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+
+[assembly: FunctionsStartup(typeof(HoaTreasurerFunctions.Startup))]
+namespace HoaTreasurerFunctions
+{
+    public class Startup : FunctionsStartup
+    {
+        //TODO fix dependency injection
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //builder.Services.AddSingleton<MongoService>();
+
+            //builder.Services.AddSingleton<IMongoService>((s) => {
+            //    return new MongoService();
+            //});
+
+            //builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+        }
+    }
+}
